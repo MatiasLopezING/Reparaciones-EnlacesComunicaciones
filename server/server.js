b9d9ebb1d104e8ba120fbcc6d3d17ce38421dffb
MatiasLopezING <ml3280199@gmail.com>
2025-07-23 20:34:32 -0300

    server update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const path = require('path');
 const db = require('./db');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 app.use(cors());
 app.use(express.json());
